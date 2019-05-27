83e7013a065938ec1cc40af9a24db1c6d016ea20
Francisco Viana <franciscoviana862@gmail.com>
2019-05-27 00:16:44 -0300

    Validacao da do form dadosAluno.xhtml

--- src/br/com/loucademia/interfaces/aluno/web/AlunoBean.java
+++ src/br/com/loucademia/interfaces/aluno/web/AlunoBean.java
@@ -4,9 +4,13 @@ import java.io.Serializable;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.loucademia.application.service.AlunoService;
+import br.com.loucademia.application.util.StringUtils;
 import br.com.loucademia.domain.aluno.Aluno;
 
 @Named
@@ -16,11 +20,25 @@ public class AlunoBean implements Serializable{
 	@EJB
 	private AlunoService alunoService;
 	
+	@Inject
+	private FacesContext facesContext;
+	
 	private Aluno aluno = new Aluno();
 	
+	private String titulo = "Novo aluno";
+	
+	private String matricula;
+	
+	public void carregar() {
+		if(!StringUtils.isEmpty(matricula)) {
+			aluno = alunoService.findByMatricula(matricula);
+			this.titulo = "Alterar aluno";
+		}
+	}
+	
 	public String gravar() {
-		System.err.println("ALUNO ==>" + aluno);
 		alunoService.createOrUpdate(aluno);
+		facesContext.addMessage(null, new FacesMessage("Dados gravados com sucesso"));
 		return null;
 	}
 
@@ -31,7 +49,18 @@ public class AlunoBean implements Serializable{
 	public void setAluno(Aluno aluno) {
 		this.aluno = aluno;
 	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
 	
+	public String getTitulo() {
+		return titulo;
+	}
 	
 
 }
